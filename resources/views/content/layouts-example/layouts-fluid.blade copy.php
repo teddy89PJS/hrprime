0e60c867d0ab6b96000dce520b925505dbd7dac5
JAY030102 <0301villasfrancisjay@gmail.com>
2025-04-23 09:19:12 +0800

    additional changes

--- resources/views/content/layouts-example/layouts-fluid.blade copy.php
+++ resources/views/content/layouts-example/layouts-fluid.blade copy.php
@@ -15,7 +15,8 @@ $containerNav = 'container-fluid';
   </div>
   <div class="layout-demo-info">
     <h4>Layout fluid</h4>
-    <p>Fluid layout sets a <code>100% width</code> at each responsive breakpoint.</p>dsadsadhghgh
+    <p>Fluid layout sets a <code>100% width</code> at each responsive breakpoint.</p>
+  </div>
 </div>
 <!--/ Layout Demo -->
 @endsection
